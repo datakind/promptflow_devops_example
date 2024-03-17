41f37e0cf18df33ee628e245b33e220aeb580dee
Matthew Harris <matthew@regolith.org>
2024-03-17 16:43:13 -0400

    Added static groundedness

---
+++ flows/reliefweb_chat/reliefweb-chat/concatenate_scores.py
@@ -0,0 +1,39 @@
+import re
+
+import numpy as np
+from promptflow import tool
+
+
+@tool
+def concat_results(groundesness_score: str):
+    """
+    Concatenates the results of groundedness scores.
+
+    Args:
+        deep_eval results (str): The evaluation score
+
+    Returns:
+        dict: A dictionary containing the concatenated results of groundedness scores.
+    """
+
+    load_list = [{"name": "gpt_groundedness", "score": groundesness_score}]
+    score_list = []
+    errors = []
+    for item in load_list:
+        try:
+            score = item["score"]
+            match = re.search(r"\d", score)
+            if match:
+                score = match.group()
+            score = float(score)
+        except Exception as e:
+            score = np.nan
+            errors.append({"name": item["name"], "msg": str(e), "data": item["score"]})
+        score_list.append({"name": item["name"], "score": score})
+
+    variant_level_result = {}
+    for item in score_list:
+        item_name = str(item["name"])
+        variant_level_result[item_name] = item["score"]
+        variant_level_result[item_name + "_pass_rate"] = 1 if item["score"] > 3 else 0
+    return variant_level_result
